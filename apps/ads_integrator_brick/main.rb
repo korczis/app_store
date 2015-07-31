bb2b1e2549077beff0c8c6c34d46fa836d156976
adriantoman <adrian.toman@gooddata.com>
2015-07-31 12:42:48 +0200

    ADS Integration Brick - v0.0.5 -  moved to different version of Metadata Gem

--- apps/ads_integrator_brick/main.rb
+++ apps/ads_integrator_brick/main.rb
@@ -6,7 +6,7 @@ fetch_gems = true
 
 repo_gems = [
   'https://gdc-ms-ruby-packages.s3.amazonaws.com/gooddata_connectors_base/s3.zip',
-  'https://gdc-ms-ruby-packages.s3.amazonaws.com/gooddata_connectors_metadata/bds_implementation.zip',
+  'https://gdc-ms-ruby-packages.s3.amazonaws.com/gooddata_connectors_metadata/v0.0.2.zip',
   'https://gdc-ms-ruby-packages.s3.amazonaws.com/gooddata_connectors_ads/v0.0.4.zip'
 ]
 
