3e331d5a2609bedcdb191efe9998add9d02009f2
adriantoman <adrian.toman@gooddata.com>
2015-11-20 13:47:03 +0100

    ADS Integrator Brick - version without dependency on rubygems.

--- apps/ads_integrator_brick/main.rb
+++ apps/ads_integrator_brick/main.rb
@@ -2,38 +2,20 @@
 
 require 'fileutils'
 
-fetch_gems = true
+debug_install = true if !$SCRIPT_PARAMS.nil? and $SCRIPT_PARAMS.include?("DEBUG_INSTALL")
 
-repo_gems = [
-  'https://gdc-ms-ruby-packages.s3.amazonaws.com/gooddata_connectors_base/s3.zip',
-  'https://gdc-ms-ruby-packages.s3.amazonaws.com/gooddata_connectors_metadata/v0.0.4.zip',
-  'https://gdc-ms-ruby-packages.s3.amazonaws.com/gooddata_connectors_ads/v0.0.6.zip'
-]
+postfix = debug_install ? "2>&1": "1>/dev/null"
 
-if fetch_gems
-  repo_gems.each do |repo_gem|
-    cmd = "curl -LOk --retry 3 #{repo_gem} 2>&1"
-    puts cmd
-    system(cmd)
+package = 'https://gdc-ms-ruby-packages.s3.amazonaws.com/ads_integrator_brick/v0.0.7.zip'
+system("curl -LOk --retry 3 #{package} #{postfix}")
 
-    repo_gem_file = repo_gem.split('/').last
-
-    cmd = "unzip -o #{repo_gem_file} 2>&1"
-    puts cmd
-    system(cmd)
-
-    FileUtils.rm repo_gem_file
-  end
-end
-
-#Create output folder
-require 'fileutils'
-FileUtils.mkdir_p('output')
+local_package = package.split('/').last
+system("unzip -o #{local_package} #{postfix}")
 
 # Bundler hack
 require 'bundler/cli'
-Bundler::CLI.new.invoke(:install, [],:path => "gems", :retry => 3, :jobs => 4,:deployment => true)
-
+Bundler::CLI.new.invoke(:install, [],:path => "gems", :retry => 3, :jobs => 4,:deployment => true,:local => true)
+#
 # Required gems
 require 'bundler/setup'
 require 'gooddata'
