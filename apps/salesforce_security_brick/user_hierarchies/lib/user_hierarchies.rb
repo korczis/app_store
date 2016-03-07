aec9c472c60b63c97f0b0020d7c9941021c3507f
Tomas Korcak <korczis@gmail.com>
2016-03-07 10:57:52 +0100

    Deleted salesforce_security_brick

--- apps/salesforce_security_brick/user_hierarchies/lib/user_hierarchies.rb
+++
@@ -1,6 +0,0 @@
-$LOAD_PATH << File.expand_path(File.dirname(File.realpath(__FILE__)))
-
-require_relative 'user_hierarchies/version'
-require_relative 'user_hierarchies/extension'
-require_relative 'user_hierarchies/user'
-require_relative 'user_hierarchies/user_hierarchy'
--- apps/salesforce_security_brick/user_hierarchies/lib/user_hierarchies/version.rb
+++
@@ -1,5 +0,0 @@
-module GoodData
-  module UserHierarchies
-    VERSION = '0.2.0'
-  end
-end
